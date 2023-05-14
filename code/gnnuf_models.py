cc28d11801be9f3c4b08e8c404ffe7bffd484b4c
sdesabbata <s.desabbata@le.ac.uk>
2023-05-14 19:06:22 +0100

    First tests

---
+++ code/gnnuf_models.py
@@ -0,0 +1,90 @@
+# Based on
+# https://github.com/pyg-team/pytorch_geometric/blob/master/torch_geometric/nn/models/autoencoder.py [MIT license]
+# https://github.com/pyg-team/pytorch_geometric/blob/master/examples/autoencoder.py [MIT license]
+# https://github.com/pyg-team/pytorch_geometric/blob/master/examples/argva_node_clustering.py [MIT license]
+
+from typing import Optional, Tuple
+import warnings
+
+import torch
+from torch_geometric.nn import GCNConv, GATConv, global_mean_pool, InnerProductDecoder
+
+
+# ------------------------ #
+#   Vanilla GCN encoder    #
+# ------------------------ #
+
+# Based on
+# https://colab.research.google.com/github/AntonioLonga/PytorchGeometricTutorial/blob/main/Tutorial6/Tutorial6.ipynb
+
+class VanillaGCNEncoder(torch.nn.Module):
+    def __init__(self, in_channels, gcn_channels, out_channels):
+        super(VanillaGCNEncoder, self).__init__()
+        self.en_linear1 = torch.nn.Linear(in_channels, in_channels)
+        self.en_conv1 = GCNConv(in_channels, gcn_channels)
+        self.en_conv2 = GCNConv(gcn_channels, gcn_channels)
+        self.en_linear2 = torch.nn.Linear(gcn_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.en_linear1(x).relu()
+        x = self.en_conv1(x, edge_index).relu()
+        x = self.en_conv2(x, edge_index).relu()
+        x = self.en_linear2(x)
+        return torch.tanh(x)
+
+
+# ---------------------- #
+#   GAT-based encoder    #
+# ---------------------- #
+
+class GATEncoder(torch.nn.Module):
+    def __init__(self,
+            layers_features_in, layers_features_prep, layers_features_gatc, layers_features_post,
+            num_gat_heads, negative_slope, dropout
+        ):
+        super(GATEncoder, self).__init__()
+
+        self.negative_slope = negative_slope
+
+        # --- Prep layers ---
+        if len(layers_features_prep) == 0:
+            self.sequential_prep = None
+        else:
+            self.sequential_prep = torch.nn.Sequential()
+            self.sequential_prep.append(torch.nn.Linear(layers_features_in, layers_features_prep[0]))
+            self.sequential_prep.append(torch.nn.LeakyReLU(negative_slope=negative_slope))
+            for i in range(len(layers_features_prep) - 1):
+                self.sequential_prep.append(torch.nn.Linear(layers_features_prep[i], layers_features_prep[i + 1]))
+                self.sequential_prep.append(torch.nn.LeakyReLU(negative_slope=negative_slope))
+
+        # --- GAT layers ---
+        self.gatconvs = torch.nn.ModuleList()
+        # GAT layers except last
+        for layers_features_gatc_h in layers_features_gatc[:-1]:
+            self.gatconvs.append(GATConv(
+                -1, layers_features_gatc_h,
+                heads=num_gat_heads, negative_slope=negative_slope, dropout=dropout
+            ))
+        # Last GAT layer, averaged instead of concatenated
+        self.gatconvs.append(GATConv(
+            -1, layers_features_gatc[-1],
+            heads=num_gat_heads, concat=False, negative_slope=negative_slope, dropout=dropout
+        ))
+
+        # --- Final layers ---
+        self.sequential_post = torch.nn.Sequential()
+        self.sequential_post.append(torch.nn.Linear(layers_features_gatc[-1], layers_features_post[0]))
+        self.sequential_post.append(torch.nn.LeakyReLU(negative_slope=negative_slope))
+        for i in range(len(layers_features_post) - 1):
+            self.sequential_post.append(torch.nn.Linear(layers_features_post[i], layers_features_post[i + 1]))
+
+        self.final_batch_norm_1d = torch.nn.BatchNorm1d(layers_features_post[-1])
+
+    def forward(self, attributes, edge_index):
+        if self.sequential_prep is not None:
+            attributes = self.sequential_prep(attributes)
+        for a_gatconv in self.gatconvs:
+            attributes = a_gatconv(attributes, edge_index)
+        attributes = self.sequential_post(attributes)
+        attributes = self.final_batch_norm_1d(attributes)
+        return attributes
---
+++ code/gnnuf_train.py
@@ -0,0 +1,125 @@
+# Base libraries
+import os
+from os.path import isfile, join
+import math
+import numpy as np
+import random
+# NetworkX
+import networkx as nx
+import osmnx as ox
+# Torch
+from torch_geometric.data import InMemoryDataset
+from torch_geometric.nn import GAE
+from torch_geometric.utils import from_networkx
+# GNN models
+from gnnuf_models import *
+# OS environment setup
+from local_directories import *
+
+# Reset random seeds
+random_seed = 2674
+random.seed(random_seed)
+np.random.seed(random_seed)
+torch.manual_seed(random_seed)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    print("Run on CUDA")
+else:
+    print("Run on CPU")
+
+
+class OSMxDataset(InMemoryDataset):
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+        super().__init__(root, transform, pre_transform, pre_filter)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        # Just check if Leicester's graph is there
+        return ["leicester-1864.graphml"]
+
+    @property
+    def processed_file_names(self):
+        return ["data.pt"]
+
+    def download(self):
+        print("Haven't got time to implement this. Please download the data manually -.- ")
+
+    def process(self):
+        graphml_file_names = [
+            join(osmnx_dir + "/raw", f)
+            for f in os.listdir(osmnx_dir + "/raw")
+            if f[-8:] == ".graphml"
+            if isfile(join(osmnx_dir + "/raw", f))]
+
+        neighbourhoods_list = []
+
+        for graphml_file_name in graphml_file_names:
+
+            print(f"Processing: {graphml_file_name}")
+
+            G = ox.io.load_graphml(graphml_file_name)
+
+            # Sample 1% of nodes
+            sample_nodes = random.sample(list(G.nodes), math.ceil(len(G.nodes) * 0.01))
+            # sample_nodes = random.sample(list(G.nodes), math.ceil(10))
+
+            for sampled_node in sample_nodes:
+
+                sampled_ego_graph = nx.generators.ego_graph(
+                    G, sampled_node,
+                    radius=2000, undirected=True, distance="length"
+                )
+
+                if len(sampled_ego_graph.nodes) > 32:
+
+                    seg_edges_length = nx.get_edge_attributes(sampled_ego_graph, "length")
+                    seg_edges_edges_grade_abs = nx.get_edge_attributes(sampled_ego_graph, "grade_abs")
+
+                    seg_linegraph = nx.line_graph(sampled_ego_graph)
+                    for seglg_node in seg_linegraph.nodes():
+                        seg_linegraph.nodes[seglg_node]["x"] = [
+                            seg_edges_length[(seglg_node[0], seglg_node[1], 0)],
+                            seg_edges_edges_grade_abs[(seglg_node[0], seglg_node[1], 0)]
+                        ]
+                    del seglg_node
+
+                    pyg_graph = from_networkx(seg_linegraph)
+                    neighbourhoods_list.append(pyg_graph)
+
+        self.data, self.slices = self.collate(neighbourhoods_list)
+        torch.save((self.data, self.slices), self.processed_paths[0])
+
+
+osmnx_dir = bulk_storage_directory + "/osmnx"
+osmnx_dataset = OSMxDataset(osmnx_dir)
+
+
+from gnnuf_models import VanillaGCNEncoder
+
+model = GAE(VanillaGCNEncoder(2, 128, 64))
+model = model.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.0001)
+
+print(model)
+
+from torch_geometric.loader import DataLoader
+osmnx_loader = DataLoader(osmnx_dataset, batch_size=32, shuffle=True)
+
+epochs = 100
+for epoch in range(1, epochs + 1):
+    print(f"{epoch=}")
+
+    model.train()
+
+    for osmnx_linegraph in osmnx_loader:
+        optimizer.zero_grad()
+        z = model.encode(osmnx_linegraph.x, osmnx_linegraph.edge_index)
+        loss = model.recon_loss(z, osmnx_linegraph.edge_index)
+        osmnx_linegraph_loss = float(loss)
+        print(f"{osmnx_linegraph_loss=}")
+        loss.backward()
+        optimizer.step()
+
+torch.save(model.state_dict(), bulk_storage_directory + "/models/osmnx_model_v1.pt")
