39ba69cfdbc393fab1ea90e7fd2b5f0646c646b7
sdesabbata <s.desabbata@le.ac.uk>
2023-05-18 01:20:15 +0100

    Updated exploratory analysis of model v0.1

--- code/osmnx_basic_stats_Leicester.py
+++ code/osmnx_basic_stats_Leicester.py
@@ -11,32 +11,32 @@ from multiprocessing import Pool
 # Load Leciester's graph
 leicester = ox.io.load_graphml(bulk_storage_directory + "/osmnx/raw_excluded/leicester-1864.graphml")
 
-leicester_basestats_df = None
+leicester_basic_stats_df = None
 neighbourhood_min_nodes = 8
 max_distance = 1000
 
 
-def get_base_stats(node):
+def get_basic_stats(node):
     # Create the corresponding ego graph
     node_ego_graph = nx.generators.ego_graph(leicester, node, radius=max_distance, undirected=True, distance="length")
     # Only keep the sampled area if it has a minimum number of nodes
     if len(node_ego_graph.nodes()) > neighbourhood_min_nodes:
-        node_base_stats = ox.stats.basic_stats(node_ego_graph)
-        node_base_stats["node_id"] = node
-        return node_base_stats
+        node_basic_stats = ox.stats.basic_stats(node_ego_graph)
+        node_basic_stats["node_id"] = node
+        return node_basic_stats
 
 if __name__ == '__main__':
     p = Pool(processes=25)
-    data = p.map(get_base_stats, [node for node in leicester.nodes])
+    data = p.map(get_basic_stats, [node for node in leicester.nodes])
     p.close()
     p.join()
 
     # Calculate base stats
-    for basestats in data:
-        if leicester_basestats_df is None:
-            leicester_basestats_df = pd.DataFrame.from_dict([basestats])
+    for basic_stats in data:
+        if leicester_basic_stats_df is None:
+            leicester_basic_stats_df = pd.DataFrame.from_dict([basic_stats])
         else:
-            leicester_basestats_df = pd.concat([leicester_basestats_df, pd.DataFrame.from_dict([basestats])])
+            leicester_basic_stats_df = pd.concat([leicester_basic_stats_df, pd.DataFrame.from_dict([basic_stats])])
 
     # Save
-    leicester_basestats_df.to_csv(this_repo_directory + "/data/leicester-1864_basestats.csv", index=False)
+    leicester_basic_stats_df.to_csv(this_repo_directory + "/data/leicester-1864_basic_stats.csv", index=False)
